7dce7218c7a3f4acc88c5593f4c3dc7981d6d8e7
allison-liu <allison.liu@columbia.edu>
2023-12-09 17:54:14 -0500

    adding routes in app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import React from 'react';
 import { ReactDOM } from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DormSelectionPage from './pages/dorm_selection_page';
-import MyRequestsPage from './pages/my_requests_page'
+import MyRequestsPage from './pages/my_requests_page';
+import Login from './pages/login';
+import CreateRequests from './pages/create_request';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/dorm_selection" element={<DormSelectionPage />}></Route>
         <Route path="/my_requests" element={<MyRequestsPage />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/create_requests" element={<CreateRequests />}></Route>
       </Routes>
     </Router>
   );

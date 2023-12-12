46e9382cb986fa905a4eae6254ba7148af4aa466
allison-liu <allison.liu@columbia.edu>
2023-12-12 03:10:30 -0500

    fix login style

--- src/pages/login_page.js
+++ src/pages/login_page.js
@@ -1,30 +1,32 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './login.css';
+
 
 function LoginPage() {
   const imageUrl = process.env.PUBLIC_URL + '/images/columbia-university-logo.png';
 
   return (
-    <div class="container">
-            <div id="title"> Columbia Service Requests </div>
-            <br/>
-            <div class="text-box">
-                <div id="uni">Uni</div>
-                <div>
-                <input type="text" name="uni" placeholder="eg1234" />
-                </div>
-            </div>
-            <div>
-              <div>
-                <button type="button" onclick="login()">Login</button>
-              </div>
-            </div>
-            
-            <div id="cu-logo">
-              <img src={imageUrl} alt="Columbia University Logo" />
-            </div>
-
+    <div className="container_login">
+      <div id="title_login"> Columbia Service Requests </div>
+      <br/>
+      <div class="text-box-login">
+          <div id="uni_login">Uni</div>
+          <div>
+          <input type="text" name="uni" placeholder="eg1234" />
           </div>
+      </div>
+      <div>
+        <div>
+          <button id="button" type="button">Login</button>
+        </div>
+      </div>
+      
+      <div id="cu-logo">
+        <img src={imageUrl} alt="Columbia University Logo" />
+      </div>
+
+    </div>
   );
 }
 

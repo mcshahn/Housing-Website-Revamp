a538399dd53b54bde8a40f26dd24d584e73f2a72
MichelleAhn <mcshahn@gmail.com>
2023-12-12 20:58:23 -0500

    commiting to pull

--- src/pages/login_page.js
+++ src/pages/login_page.js
@@ -1,9 +1,28 @@
-// import logo from './logo.svg';
 import React from 'react';
 import './login.css';
-
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function LoginPage() {
+  const navigate = useNavigate();
+
+  const toDormSelection=()=>{
+    navigate('/dorm_selection');
+  }
+  // const getInitialState = () => {
+  //     const value = "none";
+  //     return value;
+  //   };
+  
+  //   const [value, setValue] = useState(getInitialState);
+  
+  //   const handleChange = (e) => {
+  //     setValue(e.target.value);
+  //   };
+
+  //   console.log(value)
+
+
   const imageUrl = process.env.PUBLIC_URL + '/images/columbia-university-logo.png';
 
   return (
@@ -18,7 +37,7 @@ function LoginPage() {
       </div>
       <div>
         <div>
-          <button id="button" type="button">Login</button>
+          <button id="button" type="button" onClick={toDormSelection}>Login</button>
         </div>
       </div>
       
